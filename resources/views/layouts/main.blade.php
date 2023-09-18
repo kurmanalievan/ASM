7383a72f5c13b96c8ff094548b89110e1ec4be89
nursezim <kurmanalieva.nurs@gmail.com>
2023-09-18 14:03:16 +0200

    Add login/registration for a single user

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,9 +25,45 @@
           <!-- <a class="nav-item nav-link" href="about.html">Resource Sharing</a> -->
           <a class="nav-item nav-link" href="/support">Language Support</a>
           <a class="nav-item nav-link" href="/profile">My Profile</a>
+          {{-- <a class="nav-link" href="{{ route('login') }}">Login</a> --}}
         </div>
+        <ul class="navbar-nav ms-auto">
+          <!-- Authentication Links -->
+          @guest
+              @if (Route::has('login'))
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  </li>
+              @endif
+
+              @if (Route::has('register'))
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                  </li>
+              @endif
+          @else
+              <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                      {{ Auth::user()->name }}
+                  </a>
+
+                  <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                         onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                          {{ __('Logout') }}
+                      </a>
+
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                          @csrf
+                      </form>
+                  </div>
+              </li>
+          @endguest
+      </ul>
       </div>
     </nav>
+    
     @yield('content')
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,18 @@ Route::get('/', function () {
 // Route::get('/tutors', function () {
 //     return view('tutors.tutors');
 // });
-Route::get('/tutors', [TutorsController::class, "index"]);
-Route::get('/discussions', [DiscussionsController::class, "index"]);
-Route::get('/tasks', [TasksController::class, "index"]);
-Route::get('/support', [LanguageController::class, "index"]);
-Route::get('/profile', [ProfileController::class, "index"]);
-Route::get('/tutors/{id}/book', [TutorsController::class, "book"]);
+Route::get('/tutors', [TutorsController::class, "index"])->middleware('auth');
+Route::get('/discussions', [DiscussionsController::class, "index"])->middleware('auth');
+Route::get('/tasks', [TasksController::class, "index"])->middleware('auth');
+Route::get('/support', [LanguageController::class, "index"])->middleware('auth');
+Route::get('/profile', [ProfileController::class, "index"])->middleware('auth');
+Route::get('/tutors/{id}/book', [TutorsController::class, "book"])->middleware('auth');
 
 
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
